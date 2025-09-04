270df12232f95cfcfb05135ccf65892f1ebd0801
melandrieteuk-max <melandrieteuk@gmail.com>
2025-09-04 21:38:29 +0700

    Create stealbrainrot.lua
    script test

---
+++ stealbrainrot.lua
@@ -0,0 +1,7 @@
+print("StealBrainrot script berhasil dijalankan dari GitHub!")
+
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "GitHub Test";
+    Text = "Loadstring sukses!";
+    Duration = 5;
+})
